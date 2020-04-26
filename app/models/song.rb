4ec932ba231ffa50b176aaa6d56c915dc83e5f40
Nina Angelo <ninacelie@gmail.com>
2020-04-26 17:32:32 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ApplicationRecord
+  validates_presence_of :title, :genre 
+  validates :released, inclusion: { in: [true, false] }
+
+  include ActiveModel::Validations
+  validates_with TitleValidator
+  validates_with ReleaseYearValidator
+end
